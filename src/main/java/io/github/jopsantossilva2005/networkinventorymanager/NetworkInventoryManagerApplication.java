c397f39d5ad462e6b61f4d43bee863794e363616
jopsantossilva2005 <jpsantossilva2005@gmail.com>
2026-07-06 11:24:50 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/jopsantossilva2005/networkinventorymanager/NetworkInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jopsantossilva2005.networkinventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jopsantossilva2005/networkinventorymanager/NetworkInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jopsantossilva2005.networkinventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
